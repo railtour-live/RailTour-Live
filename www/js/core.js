6a0fc820a5d6c4b4ec0b3f70cdceb50d90358f73
Glyn Murray <g.murray@railtour-live.co.uk>
2012-06-25 12:43:10 +0100

    added tour lists

---
+++ www/js/core.js
@@ -0,0 +1,282 @@
+var state = new Object()
+state.name = '';
+state.credits = '';
+state.last = '';
+state.email = '';
+state.phone = '';
+state.user = '';
+state.password = '';
+state.phone = '';
+state.confirmed = '';
+state.sms = '';
+state.code = '';
+state.registered = '';
+state.tours = '';
+state.promook = '';
+
+
+function resetvars() {
+    state.name = '';
+    state.credits = '';
+    state.last = '';
+    state.email = '';
+    state.phone = '';
+    state.user = '';
+    state.password = '';
+    state.phone = '';
+    state.confirmed = '';
+    state.sms = '';
+    state.code = '';
+    state.registered = '';
+    state.tours = '';
+    state.promook = '';
+}
+
+//var activeSection = 'HOME';
+var GLOBAL_SERVER = 'https://www.railtour-live.co.uk/app/mobile.php';
+var appBusy = false;
+
+function preventBehavior(e) 
+{ 
+    e.preventDefault(); 
+};
+
+//window.plugins.inAppPurchaseManager.onPurchased = function(transactionIdentifier, productId, transactionReceipt) {
+//    console.log('purchased: ' + productId);
+//    /* Give coins, enable subscriptions etc */
+//}
+
+//window.plugins.inAppPurchaseManager.onRestored = function(transactionIdentifier, productId, transactionReceipt) {
+//    console.log('restored: ' + productId);
+//    /* See the developer guide for details of what to do with this */
+//}
+
+//window.plugins.inAppPurchaseManager.onFailed = function(errno, errtext) {
+//    console.log('failed: ' + errtext);
+//}
+
+//window.plugins.inAppPurchaseManager.requestProductData("com.example.test", function(productId, title, description, price) {
+//                                                       console.log("productId: " + productId + " title: " + title + " description: " + description + " price: " + price);
+//                                                       window.plugins.inAppPurchaseManager.makePurchase(productId, 1);
+//                                                       }, function(id) {
+//                                                       console.log("Invalid product id: " + id);
+//                                                       }
+//                                                       );
+
+
+
+document.addEventListener("touchmove", preventBehavior, false);
+
+$("#pageHome").live('pagehide', forcerefresh);
+$("#pageLogin").live('pagehide', forcerefresh);
+$("#pageTours").live('pagehide', forcerefresh);
+$("#pageProfile").live('pagehide', forcerefresh);
+$("#pageSettings").live('pagehide', forcerefresh);
+$("#pageUpdates").live('pagehide', forcerefresh);
+$("#pageHelp").live('pagehide', forcerefresh);
+$("#pageConfirm").live('pagehide', forcerefresh);
+
+$("#pageHome").live('pagebeforecreate', addmarkup);
+$("#pageLogin").live('pagebeforecreate', loginaddmarkup);
+$("#pageTours").live('pagebeforecreate', addmarkup);
+$("#pageProfile").live('pagebeforecreate', addmarkup);
+$("#pageSettings").live('pagebeforecreate', addmarkup);
+$("#pageUpdates").live('pagebeforecreate', addmarkup);
+$("#pageHelp").live('pagebeforecreate', addmarkup);
+$("#pageConfirm").live('pagebeforecreate', loginaddmarkup);
+
+
+$(document).bind('pagechange', beforechange);
+
+function onDeviceReady() {
+	initialized=true;
+    
+}
+
+
+function getheader() {
+	
+	var header = '';
+    
+	if (IsLoggedIn() == false) {
+		header = header + '<a href="info.html" data-iconpos="notext" id="help" data-icon="info" data-theme="b"></a><H1>Login</H1>'
+	} else {
+		header = header
+        + '<a href="info.html" data-iconpos="notext" id="help" data-icon="info" data-theme="b"></a><H2>Credit &pound;'
+        + state.credits
+        + '</H2><a href="login.html" data-role="button" data-theme="b" onClick="doLogout()">Logout</a>';
+	}
+	return (header);
+}
+
+function getfooter() {
+    
+    var actHome='';
+    var actTours='';
+    var actProfile='';
+    var actSettings='';
+    var actUpdates='';
+    var badge='';
+    var promo='';
+    if (activeSection=='HOME') actHome='class="ui-btn-active"';
+    if (activeSection=='TOURS') actTours='class="ui-btn-active"';
+    if (activeSection=='PROFILE') actProfile='class="ui-btn-active"';
+    if (activeSection=='SETTINGS') actSettings='class="ui-btn-active"';
+    if (activeSection=='UPDATES') actUpdates='class="ui-btn-active"';
+    badge = '0';
+	promo = '0';
+    var foot = '<div data-role="navbar" class="nav-glyphish-example" data-grid="d">\
+    <ul class="ui-grid-b">\
+    <li class="ui-block-a"><a href="#" onClick="goHome();" data-theme="b" '+actHome+' data-role="button" id="Bhome" data-icon="custom">Home</a></li>\
+    <li class="ui-block-b"><a href="#" onClick="goTours();" data-theme="b" '+actTours+' data-role="button" id="Btours" data-icon="custom">Tours</a></li>\
+    <li class="ui-block-c"><a href="#" onClick="goProfile();" data-theme="b" '+actProfile+' data-role="button" id="Bprofile" data-icon="custom">Profile</a></li>\
+    <li class="ui-block-d"><a href="#" onClick="goSettings();" data-theme="b" '+actSettings+' data-role="button" id="Bsettings" data-icon="custom">Settings</a></li>\
+    <li class="ui-block-e"><a href="#" onClick="goUpdates();" data-theme="b" '+actUpdates+' data-role="button" id="Bupdate" data-icon="custom">\
+    <div class="badger-outter" id="Badger"><div class="badger-inner"><p class="badger-badge" id="Badge">'+badge+'</p></div></div>Updates</a></li>\
+    </ul>\
+    </div>';
+
+    return(foot);
+    
+}
+
+function forcerefresh(event, ui){
+	var page = jQuery(event.target);
+	page.remove();
+    
+}
+
+function loginaddmarkup(event,data) {
+	addmarkup(event,data);
+    $('.ui-block-b').addClass('ui-disabled');
+	$('.ui-block-c').addClass('ui-disabled');
+	$('.ui-block-d').addClass('ui-disabled');
+	$('.ui-block-e').addClass('ui-disabled');
+}
+
+function addmarkup(event, data) {
+	appBusy = false;
+	
+	var page = $(event.handleObj.selector);
+	var header = page.find("#mainheader").first();
+	header.html(getheader);
+    
+	var footer = page.find("#mainfooter").first();
+	footer.html(getfooter(''));
+    footer.fixedtoolbar({
+                        tapToggle : false
+                        });
+}
+
+function beforechange(event, data) {
+	if ($.mobile.activePage != undefined)
+		currentpageid = ($.mobile.activePage).attr('id');
+}
+
+function doLogout() {
+	state.name = '';
+	resetvars();
+	searchcount=0;
+	$('#contentLoginInfo').html('&nbsp;');
+    $('.ui-block-b').addClass('ui-disabled');
+    $('.ui-block-c').addClass('ui-disabled');
+    $('.ui-block-d').addClass('ui-disabled');
+    $('.ui-block-e').addClass('ui-disabled');
+}
+
+function IsLoggedIn() {
+	if (state.name == '') {
+        $('.ui-block-b').addClass('ui-disabled');
+        $('.ui-block-c').addClass('ui-disabled');
+        $('.ui-block-d').addClass('ui-disabled');
+        $('.ui-block-e').addClass('ui-disabled');
+		return (false);
+	} else
+		return (true);
+}
+
+function showspinner(msg) {
+	$.mobile.loadingMessageTextVisible = true;
+	$.mobile.loadingMessageTheme = 'a';
+	$.mobile.loadingMessage = msg;
+	$.mobile.showPageLoadingMsg();
+}
+
+function hidespinner() {
+	$.mobile.loadingMesasge = 'Loading...';
+	$.mobile.hidePageLoadingMsg();
+    
+}
+
+function setupAjaxErrorHandler(dest) {
+	$.ajaxSetup({
+                error : function(x, e) {
+                $(dest).css("color", " red");
+                if (x.status == 0) {
+				$(dest).html('You are offline!!\n Please Check Your Network.');
+                } else if (x.status == 404) {
+				$(dest).html('Requested URL not found.');
+                } else if (x.status == 500) {
+				$(dest).html('Internal Server Error.');
+                } else if (e == 'parsererror') {
+				$(dest).html('Error.\nParsing JSON Request failed.');
+                } else if (e == 'timeout') {
+				$(dest).html('Request Time out.');
+                } else {
+				$(dest).html(x.responseText);
+                }
+                hidespinner();
+                }
+                
+                });
+};
+
+function serializeSession($form, $action) {
+	var q = $form.serialize();
+	q = q + "&username=" + state.user + "&password=" + state.password
+    + "&action=" + $action;
+	return (q);
+}
+
+
+function goHome() {
+	if (!IsLoggedIn()) {
+		$.mobile.changePage('login.html');
+	} else 	$.mobile.changePage('home.html');
+}
+
+function goProfile() {
+	if (state.name == "") {
+		$.mobile.changePage('login.html');
+		return true;
+	} else {
+		$.mobile.changePage('profile.html');
+	}	
+}
+
+function goSettings() {
+	if (state.name == "") {
+		$.mobile.changePage('login.html');
+		return true;
+	} else {
+		$.mobile.changePage('settings.html');
+	}	
+}
+
+function goTours() {
+//	if (state.name == "") {
+//		$.mobile.changePage('login.html');
+//		return true;
+//	} else {
+		$.mobile.changePage('tours.html');
+//	}	
+}
+
+function getStoredUser() {
+	return(window.localStorage.getItem("User"));
+}
+
+function setStoredUser(user) {
+	window.localStorage.setItem("User", user);
+    
+}
---
+++ www/js/header.js
@@ -0,0 +1,13 @@
+var platform='ios';
+function writeheader(formaps) {
+    document.write('<title></title>\
+                   <meta http-equiv="Content-type" content="text/html; charset=utf-8">\
+                   <link href="rtl.css" rel="stylesheet" type="text/css" media="screen"/>\
+                   <link rel="stylesheet" href="badger.css" type="text/css" />');
+                   document.write('<script type="text/javascript" charset="utf-8" src="js/jquery-1.7.1.min.js"></script>\
+                                  <script type="text/javascript" charset="utf-8" src="js/jquery.mobile-1.1.0.js"></script>\
+                                  <script type="text/javascript" charset="utf-8" src="js/cordova-1.8.0.js"></script>\
+                                  <script type="text/javascript" src="js/badger.js"></script>\
+                                  <script type="text/javascript" charset="utf-8" src="js/core.js"></script>\
+                   ');
+                                  }
